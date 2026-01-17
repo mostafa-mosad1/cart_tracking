d5621eb14e0a3a9bebb8eb292ee265db5da776ae
mostafa mosad <118058556+mostafa-mosad1@users.noreply.github.com>
2026-01-17 20:01:08 +0200

    Create section/home.tsx

---
+++ section/home.tsx
@@ -0,0 +1,16 @@
+{
+  "name": "Text_With_Image",
+  "settings": [
+    {
+      "id": "textarea",
+      "label": "sssssssssssssss",
+      "type": "textarea",
+      "default": "Enter description here"
+    }
+  ],
+  "presets": [
+    {
+      "name": "Text_with_Image"
+    }
+  ]
+}
